e702ad6ffb9568f4473bfa2d022f37b023606c65
Wanja Sipinski <wanja@sipinski.de>
2017-10-08 18:50:50 +0200

    Save Events to file

--- iosstatsparser.py
+++ iosstatsparser.py
@@ -6,10 +6,20 @@ statsfile = "demofile.json"
 with open(statsfile) as datafile:
     statsdata = json.load(datafile)
 
+for event in statsdata["matchData"]["matchEvents"]:
+    if not event["event"] == "(null)":
+        print(
+        event["event"], ",", 
+        event["period"], ",",
+        event["player1SteamId"], ",",
+        event["player2SteamId"], ",", 
+        event["second"], ",",
+        event["team"],
+        "\n", sep='')
 
 
-for player in statsdata["matchData"]["players"]:
-    pprint(player["info"]["steamId"])
+#for player in statsdata["matchData"]["players"]:
+    #pprint(player["info"]["steamId"])
 
-print(statsdata["matchData"]["matchInfo"]["periods"])
+#print(statsdata["matchData"]["matchInfo"]["periods"])
 
