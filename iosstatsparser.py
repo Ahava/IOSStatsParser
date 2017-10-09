a3737c5f37e9f6970899c3d0d70c037fa910e230
Wanja Sipinski <wanja@sipinski.de>
2017-10-09 08:52:13 +0200

    Rework events output

--- iosstatsparser.py
+++ iosstatsparser.py
@@ -9,13 +9,13 @@ with open(statsfile) as datafile:
 for event in statsdata["matchData"]["matchEvents"]:
     if not event["event"] == "(null)":
         print(
-        event["event"], ",", 
-        event["period"], ",",
-        event["player1SteamId"], ",",
-        event["player2SteamId"], ",", 
-        event["second"], ",",
-        event["team"],
-        "\n", sep='')
+        event["event"],
+        event["period"],
+        event["player1SteamId"],
+        event["player2SteamId"],
+        event["second"],
+        event["team"], 
+        sep=',')
 
 
 #for player in statsdata["matchData"]["players"]:
