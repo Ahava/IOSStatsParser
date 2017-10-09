5017c2f2fb1f352e9d4a331bba9baa6dc41fbeb2
Wanja Sipinski <wanja@sipinski.de>
2017-10-09 10:01:52 +0200

    Work on Teamdata

--- iosstatsparser.py
+++ iosstatsparser.py
@@ -6,17 +6,76 @@ statsfile = "demofile.json"
 with open(statsfile) as datafile:
     statsdata = json.load(datafile)
 
-for event in statsdata["matchData"]["matchEvents"]:
-    if not event["event"] == "(null)":
-        print(
-        event["event"],
-        event["period"],
-        event["player1SteamId"],
-        event["player2SteamId"],
-        event["second"],
-        event["team"], 
-        sep=',')
 
+def eventData():
+    for event in statsdata["matchData"]["matchEvents"]:
+        if not event["event"] == "(null)":
+            print(
+                event["event"],
+                event["period"],
+                event["player1SteamId"],
+                event["player2SteamId"],
+                event["second"],
+                event["team"], 
+                sep=','
+            )
+    
+    print("\n")
+
+def teamData():
+    print(
+        "Team Name",
+        "Side",
+        "Is Mix",
+        "Red Cards",
+        "Yellow Cards",
+        "Fouls",
+        "Fouls Suffered",
+        "Sliding Tackles",
+        "Sliding Tackles Completed",
+        "Goals Conceded",
+        "Shots",
+        "Shots on Goal",
+        "Passes Completed",
+        "Interceptions",
+        "Offsides",
+        "Goals",
+        "Own Goals",
+        "Assists",
+        "Passes",
+        "Free Kicks",
+        "Penalties",
+        "Corners",
+        "Throw Ins",
+        "Keeper Saves",
+        "Goal Kicks",
+        "Possession",
+        "Distance Covered",
+        "Keeper Saves Caught",
+        sep=","
+    )
+
+    for teams in statsdata["matchData"]["teams"]:
+        totalstats = ""
+        name = str(teams["matchTotal"]["name"])
+        side = str(teams["matchTotal"]["side"])
+        isMix = str(teams["matchTotal"]["isMix"])
+
+        totalstats = name + "," + side + "," + isMix + ","
+
+
+        for stat in teams["matchTotal"]["statistics"]:
+            statsstring = str(stat)
+            statsstring.replace(" ", "")
+            totalstats = totalstats + statsstring + ","
+
+        totalstats = totalstats[:-1]
+        print(totalstats)
+
+    print("\n")
+
+eventData()
+teamData()
 
 #for player in statsdata["matchData"]["players"]:
     #pprint(player["info"]["steamId"])
